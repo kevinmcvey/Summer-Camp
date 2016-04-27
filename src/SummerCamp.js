a6738bf4a6f009698615ce660ec2652c0b64a30b
kevinmcvey <mcveymk@gmail.com>
2016-04-26 22:47:02 -0700

    Improve framerate and set up a fade-in portion

--- src/SummerCamp.js
+++ src/SummerCamp.js
@@ -1,7 +1,10 @@
-var FADE_INTERVAL = 100;
-var FADE_STEPS = 100;
-var DEFAULT_DAY_LIMIT = 10;
-var TITLE_DISPLAY = 1000;
+var FPS = 20;
+var FADE_INTERVAL = Math.ceil(1000 / FPS);
+var FADE_OUT_TIME = 10; // seconds
+var FADE_IN_TIME = 1; // seconds
+var FADE_OUT_STEPS = Math.ceil((FADE_OUT_TIME * 1000) / FADE_INTERVAL);
+var FADE_IN_STEPS = Math.ceil((FADE_IN_TIME * 1000) / FADE_INTERVAL);
+var DEFAULT_DAY_LIMIT = 10; // days
 
 function SummerCamp(layerController, coverContainer, $titleContainer, dayLimit) {
   if (window === this) {
@@ -11,7 +14,8 @@ function SummerCamp(layerController, coverContainer, $titleContainer, dayLimit) 
   this.layerController = layerController;
   this.coverContainer = coverContainer;
 
-  this.fadeOpacity = 0.0;
+  this.fadeOpacity = 1.0;
+  this.fadeStepsPassed = 0;
   this.scheduler = layerController.scheduler; // TODO refactor?
   this.dayLimit = dayLimit || DEFAULT_DAY_LIMIT;
   this.daysPassed = 0;
@@ -33,19 +37,28 @@ SummerCamp.prototype.initialize = function() {
   this.demoStep = true;
 
   // This is where I have to add the title updater thing
-  this.timePassedId = this.scheduler.schedule(FADE_INTERVAL, this.onTimePassed.bind(this));
-  this.dayPassedId = this.scheduler.schedule(FADE_INTERVAL * FADE_STEPS, this.onDayPassed.bind(this));
+  //this.fadeInId = this.scheduler.schedule(FADE_INTERVAL, this.setFadeIn.bind(this));
+  this.fadeOutId = this.scheduler.schedule(FADE_INTERVAL, this.setFade.bind(this));
+  //this.dayPassedId = this.scheduler.schedule(FADE_INTERVAL * (FADE_OUT_STEPS + FADE_IN_STEPS), this.onDayPassed.bind(this));
 };
 
-SummerCamp.prototype.onTimePassed = function() {
+SummerCamp.prototype.setFade = function() {
   // TODO: Should this live in layer controller? Given that it's a project-wide
   // fade layer I'm not so sure.
   this.layerController.updateCoverOpacity(this.fadeOpacity);
-  if (this.demoStep) {
-    this.title.setOpacity(1 - this.fadeOpacity);
+
+  if (this.fadeStepsPassed < FADE_IN_STEPS) {
+      this.fadeOpacity -= (1 / FADE_IN_STEPS);
+      this.fadeStepsPassed++;
+  } else if (this.fadeStepsPassed < (FADE_IN_STEPS + FADE_OUT_STEPS)) {
+      this.fadeOpacity += (1 / FADE_OUT_STEPS);
+      this.fadeStepsPassed++;
+  } else {
+      this.onDayPassed();
+      this.fadeStepsPassed = 0;
   }
 
-  this.fadeOpacity += (1 / FADE_STEPS);
+  this.title.setOpacity(1 - this.fadeOpacity);
 };
 
 SummerCamp.prototype.onDayPassed = function() {
@@ -66,7 +79,6 @@ SummerCamp.prototype.onDayPassed = function() {
   this.layerController.returnAllLayersToStart();
   this.layerController.replayAllLayerLogs();
   this.updateTitle(this.daysPassed);
-  this.title.setOpacity(1.0);
 
   var randomColor = 'rgba(' + Math.floor(Math.random() * 255) + ', ' +
                               Math.floor(Math.random() * 255) + ', ' +
@@ -76,7 +88,6 @@ SummerCamp.prototype.onDayPassed = function() {
   if (this.daysPassed < this.dayLimit) {
     this.layerController.addLayer(undefined, undefined, undefined, undefined,
                                   randomColor, randomColor);
-    //                         'rgba(150, 255, 150, 1.0)', 'rgba(150, 255, 150, 0.0)');
     this.layerController.transitionActiveLayer(this.layerController.layers.length - 1);
   }
 
@@ -85,7 +96,8 @@ SummerCamp.prototype.onDayPassed = function() {
     this.layerController.cover.unbind();
   }
 
-  this.fadeOpacity = 0.0;
+  this.title.setOpacity(1.0);
+  this.fadeOpacity = 1.0;
 };
 
 SummerCamp.prototype.updateTitle = function(day) {
